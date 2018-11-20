5c59cd0c90977cf048fd4e0bb224d989126b3999
yunchuan <tangyc@zgjzd.cn>
2018-11-20 15:27:08 +0800

    1.Action-Sheet实现弹出;2.加载默认设置、缓存读取控制Action-Sheet;3.ActionSheet事件及占位符正则替换;4.CSS环形动画原理及实现;5.JS定时器是实现自动动画;6.实现数字计时及语音提示

--- app.js
+++ app.js
@@ -3,28 +3,63 @@ App({
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function () {
-    
+  onLaunch: function() {
+    //调用API从本地缓存中获取数据
+    var configs = wx.getStorageSync('configs');
+    if (!configs) {
+      configs = this.initConfigs();
+    }
+    wx.setStorageSync('configs', configs);
   },
+  initConfigs: function() {
+    var configs = new Object();
 
-  /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
-   */
-  onShow: function (options) {
-    
-  },
+    var config1 = new Object();
+    config1.id = "config1";
+    config1.name = "立论阶段";
+    config1.state = true;
+    config1.time = 180;
+    config1.voice = 15;
+    config1.desc = "\n\n（一）正方一辩开篇立论，@\n（二）反方一辩开篇立论，@";
+    configs.config1 = config1;
 
-  /**
-   * 当小程序从前台进入后台，会触发 onHide
-   */
-  onHide: function () {
-    
-  },
 
-  /**
-   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-   */
-  onError: function (msg) {
-    
+    var config2 = new Object();
+    config2.id = "config2";
+    config2.name = "驳立论阶段";
+    config2.state = true;
+    config2.time = 120;
+    config2.voice = 15;
+    config2.desc = "\n\n（一）反方二辩驳对方立论，@\n（二）正方二辩驳对方立论，@";
+    configs.config2 = config2;
+
+    var config3 = new Object();
+    config3.id = "config3";
+    config3.name = "质辩环节";
+    config3.state = true;
+    config3.time = 90;
+    config3.voice = 15;
+    config3.desc = "\n\n（一）正方三辩提问反方一、二、四辩各一个问题，反方辩手分别应答。三个问题累计回答时间为@。\n（二）反方三辩提问正方一、二、四辩各一个问题，正方辩手分别应答。三个问题累计回答时间为@。";
+    configs.config3 = config3;
+
+    var config4 = new Object();
+    config4.id = "config4";
+    config4.name = "自由辩论";
+    config4.state = true;
+    config4.time = 240;
+    config4.voice = 15;
+    config4.desc = "\n\n（一）自由辩论 @";
+    configs.config4 = config4;
+
+    var config5 = new Object();
+    config5.id = "config5";
+    config5.name = "总结陈词";
+    config5.state = true;
+    config5.time = 180;
+    config5.voice = 15;
+    config5.desc = "\n\n（一）反方四辩总结陈词，@。\n（二）正方四辩总结陈词，@。";
+    configs.config5 = config5;
+
+    return configs;
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,218 @@
 // pages/index/index.js
 Page({
-
+  leftMove: 0,
+  rightMove: 0,
   /**
    * 页面的初始数据
    */
   data: {
-
+    actionSheetHidden: true,
+    actionSheetItems: [],
+    title: "",
+    desc: "",
+    voice: 0,
+    leftAnimationData: {},
+    rightAnimationData: {},
+    leftTime: 0,
+    rightTime: 0,
+    src: '/assets/sound/countdown.mp3',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    var configs = wx.getStorageSync('configs');
+    var actionSheetItems = [];
+    var first = true;
+    for (var i in configs) {
+      var config = configs[i];
+      if (config.state) {
+        if (first) {
+          var desc = config.desc.replace(/@/g, config.time + "秒");
+          this.setData({
+            title: config.name,
+            desc: desc,
+            leftTime: config.time,
+            rightTime: config.time,
+            voice: config.voice
+          });
+          first = false;
+        }
+        actionSheetItems.push({
+          name: config.name,
+          id: config.id
+        });
+      }
+    }
+    this.setData({
+      actionSheetItems: actionSheetItems
+    });
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+  actionSheetTap: function(e) {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    })
+  },
+  actionSheetChange: function(e) {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    })
+  },
+  bindItemTap: function(e) {
+    this.leftStop();
+    this.rightStop();
+    var id = e.target.id;
+    var configs = wx.getStorageSync('configs');
+    var config = configs[id];
+    var desc = config.desc.replace(/@/g, config.time + "秒");
+    this.setData({
+      title: config.name,
+      desc: desc,
+      actionSheetHidden: true,
+      leftTime: config.time,
+      rightTime: config.time,
+      voice: config.voice
+    });
+  },
+  leftStop: function() {
+    clearInterval(this.leftInterval);
+    this.leftInterval = 0;
+    this.audioPause();
+  },
+  rightStop: function() {
+    clearInterval(this.rightInterval);
+    this.rightInterval = 0;
+    this.audioPause();
+  },
+  leftStart: function() {
+    this.rightStop();
+    if (this.leftInterval && this.leftInterval != 0) {
+      this.leftStop();
+      return;
+    }
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+    animation.rotate(this.leftMove += 100).step();
+    this.setData({
+      leftAnimationData: animation.export()
+    })
+    var page = this;
+    var leftInterval = setInterval(function() {
+      if (page.data.leftTime <= 0) {
+        page.leftStop();
+        return;
+      }
+      if (page.data.leftTime <= page.data.voice) {
+        page.audioPlay();
+      }
+      animation.rotate(page.leftMove += 100).step();
+      page.setData({
+        leftAnimationData: animation.export()
+      });
+      page.setData({
+        leftTime: page.data.leftTime - 1
+      });
+    }, 1000);
+    this.leftInterval = leftInterval;
+
+  },
+  rightStart: function() {
+    this.leftStop();
+    if (this.rightInterval && this.rightInterval != 0) {
+      this.rightStop();
+      return;
+    }
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+    animation.rotate(this.rightMove += 100).step();
+    this.setData({
+      rightAnimationData: animation.export()
+    });
+    var page = this;
+    var rightInterval = setInterval(function() {
+      if (page.data.rightTime <= 0) {
+        page.rightStop();
+        return;
+      }
+      if (page.data.rightTime <= page.data.voice) {
+        page.audioPlay();
+      }
+      animation.rotate(page.rightMove += 100).step();
+      page.setData({
+        rightAnimationData: animation.export()
+      });
+      page.setData({
+        rightTime: page.data.rightTime - 1
+      });
+    }, 1000);
+    this.rightInterval = rightInterval;
+  },
+  audioPlay: function() {
+    this.setData({
+      action: {
+        method: 'play'
+      }
+    })
+  },
+  audioPause: function() {
+    this.setData({
+      action: {
+        method: 'pause'
+      }
+    })
   }
 })
